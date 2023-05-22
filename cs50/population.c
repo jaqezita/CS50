1a4e1f3225bb2bfb2b7a61270331cdf499635d17
Jaque <jaque_hp@ghotmail.com>
2023-05-22 09:21:10 -0300

    update population solution

--- cs50/population.c
+++ cs50/population.c
@@ -4,8 +4,20 @@
 int main(void)
 {
     // TODO: Prompt for start size
+    int start;
+    do
+    {
+        start = get_int("start population size: ");
+    }
+    while (start < 9);
 
     // TODO: Prompt for end size
+    int end;
+    do
+    {
+        end = get_int("end population size: ");
+    } 
+    while (start >= end);
 
     // TODO: Calculate number of years until we reach threshold
 
